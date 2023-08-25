25e1549f3115e2c16c3da45bffcad51e5002a28f
Qoo-330ml <51946091+Qoo-330ml@users.noreply.github.com>
2023-08-25 21:14:39 +0800

    Update caddy_for_wechat.sh

--- caddy_for_wechat.sh
+++ caddy_for_wechat.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # 安装 Caddy
-echo "正在安装 Caddy..."
+echo "正在更新软件源..."
 apt update
 apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor | tee /usr/share/keyrings/caddy-archive-keyring.gpg >/dev/null 2>&1
 echo "deb [signed-by=/usr/share/keyrings/caddy-archive-keyring.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | tee /etc/apt/sources.list.d/caddy.list
 apt update
+echo "正在安装 Caddy..."
 apt install -y caddy
 
+echo "开始配置微信代理..."
 # 获取用户输入的域名或 IP 地址
 read -p "是否使用域名？(y/n): " use_domain
 
@@ -19,7 +21,7 @@ else
 fi
 
 # 获取用户选择的端口
-read -p "请输入端口号 (默认为80): " port
+read -p "请输入端口号 (默认为80，如被占用请更换): " port
 port=${port:-80}
 
 # 配置 Caddy
