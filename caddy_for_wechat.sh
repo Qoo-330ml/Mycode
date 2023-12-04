195f4441d3ed96fb07be7a1ada2e892a5a692c53
Qoo-330ml <51946091+Qoo-330ml@users.noreply.github.com>
2023-12-04 19:02:26 +0800

    Update caddy_for_wechat.sh

--- caddy_for_wechat.sh
+++ caddy_for_wechat.sh
@@ -7,69 +7,70 @@ apt install -y debian-keyring debian-archive-keyring apt-transport-https
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor | tee /usr/share/keyrings/caddy-archive-keyring.gpg >/dev/null 2>&1
 echo "deb [signed-by=/usr/share/keyrings/caddy-archive-keyring.gpg] https://dl.cloudsmith.io/public/caddy/stable/deb/debian any-version main" | tee /etc/apt/sources.list.d/caddy.list
 apt update
-echo "正在安装 Caddy..."
+echo "正在安装 Caddy..."  
 apt install -y caddy
 
 # 检测Caddy是否已安装
 if ! command -v caddy &> /dev/null; then
-    echo "Caddy未被正确安装，请检查网络并重新安装Caddy。"
+    echo "Caddy未被正确安装,请检查网络并重新安装Caddy。"
     exit 1
 fi
 
-echo "Caddy已安装，继续配置微信代理..."
+echo "Caddy已安装,继续配置代理..."
 
-# 创建 /etc/caddy/vhost 目录
-mkdir -p /etc/caddy/vhost
+# 获取用户输入
+read -p "请选择要代理的应用(1:微信 2:Telegram 3:其他): " app
 
-# 创建 /etc/caddy/vhost/wechat.conf 文件
-touch /etc/caddy/vhost/wechat.conf
-
-
-echo "开始配置微信代理..."
-# 获取用户输入的域名或 IP 地址
-read -p "是否使用域名？(y/n): " use_domain
-
-if [ "$use_domain" == "y" ]; then
-    read -p "请输入您的域名: " domain
-else
-    ip_address=$(curl -sS ipinfo.io/ip)
+# 根据选择生成配置
+if [ "$app" == "1" ]; then
+  filename="wechat"
+  proxy_url="https://qyapi.weixin.qq.com"
+elif [ "$app" == "2" ]; then
+  filename="telegram"
+  proxy_url="https://api.telegram.org"  
+elif [ "$app" == "3" ]; then
+  read -p "请输入代理名称:" filename
+  read -p "请输入接口地址:" proxy_url
 fi
 
-# 获取用户选择的端口
-read -p "请输入端口号 (默认为80，如被占用请更换): " port
+read -p "是否使用域名?(y/n): " use_domain
+
+if [ "$use_domain" == "y" ]; then
+  read -p "请输入您的域名: " domain
+else
+  ip_address=$(curl -sS ipinfo.io/ip)  
+fi
+
+# 获取端口号
+read -p "请输入端口号: " port
 port=${port:-80}
 
-# 配置 Caddy
-echo "正在将微信代理配置写入 Caddyfile..."
-cat <<EOF >/etc/caddy/vhost/wechat.conf
-EOF
+# 生成对应的Caddyfile配置文件
+mkdir -p /etc/caddy/vhost  
+touch /etc/caddy/vhost/${filename}.conf
 
-if [ "$use_domain" == "y" ]; then
-    echo "${domain}:${port} {" >> /etc/caddy/vhost/wechat.conf
-else
-    echo ":${port} {" >> /etc/caddy/vhost/wechat.conf
-fi
-
-echo "    reverse_proxy https://qyapi.weixin.qq.com {" >> /etc/caddy/vhost/wechat.conf
-echo "        header_up Host {upstream_hostport}" >> /etc/caddy/vhost/wechat.conf
-echo "    }" >> /etc/caddy/vhost/wechat.conf
-echo "}" >> /etc/caddy/vhost/wechat.conf
-
-cat <<EOF >>/etc/caddy/Caddyfile
-{
-    import /etc/caddy/vhost/*.conf
+# 写入配置
+cat <<EOF > /etc/caddy/vhost/${filename}.conf
+${domain}:${port} {
+   reverse_proxy ${proxy_url} {
+        header_up Host {upstream_hostport}
+   }
 }
 EOF
 
-# 启动 Caddy
-echo "启动 Caddy..."
-systemctl enable caddy --now
+# 导入配置
+cat <<EOF >>/etc/caddy/Caddyfile
+{
+   import /etc/caddy/vhost/${filename}.conf
+}
+EOF
 
-echo "Caddy微信代理，安装完成！"
+# 启动Caddy
+systemctl enable caddy --now
 
 # 返回结果
 if [ "$use_domain" == "y" ]; then
-    echo "您的代理接口为 http://${domain}:${port}"
+   echo "您选择的应用是${app},代理接口为 http://${domain}:${port}"
 else
-    echo "您的代理接口为 http://${ip_address}:${port}"
+   echo "您选择的应用是${app},代理接口为 http://${ip_address}:${port}"
 fi
